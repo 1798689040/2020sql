b69e36763d595850582ea99f1cc6f083228b23a7
1798689040 <67767410+1798689040@users.noreply.github.com>
2020-07-05 18:23:33 +0800

    2020.7.5.18.33

---
+++ crebas.sql
@@ -0,0 +1,334 @@
+/*==============================================================*/
+/* DBMS name:      MySQL 5.0                                    */
+/* Created on:     2020/7/5 10:04:28                            */
+/*==============================================================*/
+
+
+drop table if exists Administrator_information_table;
+
+drop table if exists Bill_collection_and_voucher_delivery_form;
+
+drop table if exists Business_information_table;
+
+drop table if exists Commodity_category_table;
+
+drop table if exists Commodity_evaluation_form;
+
+drop table if exists Commodity_list;
+
+drop table if exists Commodity_order;
+
+drop table if exists Coupon_holding_form;
+
+drop table if exists Coupon_information_sheet;
+
+drop table if exists Delivery_address_list;
+
+drop table if exists Full_reduction_scheme_table;
+
+drop table if exists Order_details;
+
+drop table if exists Rider_entry;
+
+drop table if exists Rider_information_sheet;
+
+drop table if exists User_information_table;
+
+/*==============================================================*/
+/* Table: Administrator_information_table                       */
+/*==============================================================*/
+create table Administrator_information_table
+(
+   Employee_ID          varchar(20) not null,
+   Employee_Name        varchar(20),
+   Login_password       varchar(20),
+   primary key (Employee_ID)
+);
+
+/*==============================================================*/
+/* Table: Bill_collection_and_voucher_delivery_form             */
+/*==============================================================*/
+create table Bill_collection_and_voucher_delivery_form
+(
+   Bus_Business_ID      varchar(20) not null,
+   Cou_Coupon_ID        varchar(20) not null,
+   Use_User_ID          varchar(20) not null,
+   User_ID              varchar(20) not null,
+   Business_ID          varchar(20) not null,
+   Coupon_ID            varchar(20) not null,
+   Required_number_of_orders varchar(20),
+   Number_of_orders     varchar(20),
+   primary key (Bus_Business_ID, Cou_Coupon_ID, Use_User_ID)
+);
+
+/*==============================================================*/
+/* Table: Business_information_table                            */
+/*==============================================================*/
+create table Business_information_table
+(
+   Business_ID          varchar(20) not null,
+   Business_Name        varchar(20),
+   Business_star        varchar(20),
+   Per_capita_consumption varchar(20),
+   Total_sales          varchar(20),
+   primary key (Business_ID)
+);
+
+/*==============================================================*/
+/* Table: Commodity_category_table                              */
+/*==============================================================*/
+create table Commodity_category_table
+(
+   Classification_number varchar(20) not null,
+   Commodity_ID         varchar(20),
+   Business_ID          varchar(20),
+   Category_column_name varchar(20),
+   Quantity_of_goods    varchar(20),
+   primary key (Classification_number)
+);
+
+/*==============================================================*/
+/* Table: Commodity_evaluation_form                             */
+/*==============================================================*/
+create table Commodity_evaluation_form
+(
+   Bus_Business_ID2     varchar(20) not null,
+   Use_User_ID          varchar(20) not null,
+   Com_Commodity_ID     varchar(20) not null,
+   Bus_Business_ID      varchar(20) not null,
+   Commodity_ID         varchar(20) not null,
+   Business_ID          varchar(20) not null,
+   User_ID              varchar(20) not null,
+   Evaluation_content   varchar(20),
+   Evaluation_date      datetime,
+   Star_rating          varchar(20),
+   Photo                varchar(20),
+   primary key (Bus_Business_ID2, Use_User_ID, Com_Commodity_ID, Bus_Business_ID)
+);
+
+/*==============================================================*/
+/* Table: Commodity_list                                        */
+/*==============================================================*/
+create table Commodity_list
+(
+   Commodity_ID         varchar(20) not null,
+   Category_number      varchar(20),
+   Commodity_Name       varchar(20),
+   Price                varchar(20),
+   primary key (Commodity_ID)
+);
+
+/*==============================================================*/
+/* Table: Commodity_order                                       */
+/*==============================================================*/
+create table Commodity_order
+(
+   Bus_Business_ID      varchar(20) not null,
+   Commodity_ID         varchar(20) not null,
+   Use_User_ID          varchar(20) not null,
+   Rid_Rider_ID         varchar(20) not null,
+   Ful_Full_minus_ID2   varchar(20) not null,
+   Cou_Coupon_ID        varchar(20) not null,
+   Ful_Full_minus_ID    varchar(20) not null,
+   Cou_Coupon_ID2       varchar(20) not null,
+   Order_ID             varchar(20) not null,
+   Business_ID          varchar(20) not null,
+   User_ID              varchar(20) not null,
+   Rider_ID             varchar(20) not null,
+   Original_amount      varchar(20),
+   Settlement_amount    varchar(20),
+   Full_minus_ID        varchar(20),
+   Coupon_ID            varchar(20) not null,
+   Order_time           datetime,
+   Time_of_service_required datetime,
+   Address_number       varchar(20),
+   Order_status         varchar(20),
+   primary key (Bus_Business_ID, Commodity_ID, Use_User_ID, Rid_Rider_ID, Ful_Full_minus_ID2, Cou_Coupon_ID, Ful_Full_minus_ID, Cou_Coupon_ID2)
+);
+
+/*==============================================================*/
+/* Table: Coupon_holding_form                                   */
+/*==============================================================*/
+create table Coupon_holding_form
+(
+   Use_User_ID          varchar(20) not null,
+   Bus_Business_ID      varchar(20) not null,
+   User_ID              varchar(20) not null,
+   Coupon_ID            varchar(20) not null,
+   Business_ID          varchar(20) not null,
+   Preferential_amount  varchar(20),
+   Number               varchar(20),
+   closing_date         datetime,
+   primary key (Use_User_ID, Bus_Business_ID)
+);
+
+/*==============================================================*/
+/* Table: Coupon_information_sheet                              */
+/*==============================================================*/
+create table Coupon_information_sheet
+(
+   Coupon_ID            varchar(20) not null,
+   Preferential_amount  varchar(20),
+   Required_number_of_orders varchar(20),
+   Start_date           datetime,
+   End_date             datetime,
+   primary key (Coupon_ID)
+);
+
+/*==============================================================*/
+/* Table: Delivery_address_list                                 */
+/*==============================================================*/
+create table Delivery_address_list
+(
+   Address_number       varchar(20) not null,
+   User_ID              varchar(20),
+   province             varchar(20),
+   city                 varchar(20),
+   area                 varchar(20),
+   address              varchar(20),
+   contacts             varchar(20),
+   phone_number         varchar(20),
+   primary key (Address_number)
+);
+
+/*==============================================================*/
+/* Table: Full_reduction_scheme_table                           */
+/*==============================================================*/
+create table Full_reduction_scheme_table
+(
+   Full_minus_ID        varchar(20) not null,
+   Full_reduction_amount varchar(20),
+   Preferential_amount  varchar(20),
+   Can_it_be_superimposed_with_coupons bool,
+   primary key (Full_minus_ID)
+);
+
+/*==============================================================*/
+/* Table: Order_details                                         */
+/*==============================================================*/
+create table Order_details
+(
+   Order_ID             varchar(20) not null,
+   Commodity_ID         varchar(20),
+   Number               varchar(20),
+   Price                varchar(20),
+   Discount_amount_of_single_product varchar(20),
+   primary key (Order_ID)
+);
+
+/*==============================================================*/
+/* Table: Rider_entry                                           */
+/*==============================================================*/
+create table Rider_entry
+(
+   Rid_Rider_ID         varchar(20) not null,
+   User_ID              varchar(20) not null,
+   Ord_Order_ID         varchar(20) not null,
+   Rider_ID             varchar(20) not null,
+   Order_ID             varchar(20) not null,
+   time                 datetime,
+   User_evaluation      varchar(20) not null,
+   Single_income        varchar(20),
+   primary key (Rid_Rider_ID, User_ID, Ord_Order_ID)
+);
+
+/*==============================================================*/
+/* Table: Rider_information_sheet                               */
+/*==============================================================*/
+create table Rider_information_sheet
+(
+   Rider_ID             varchar(20) not null,
+   Rider_Name           varchar(20),
+   Entry_date           datetime,
+   Rider_status         varchar(20),
+   primary key (Rider_ID)
+);
+
+/*==============================================================*/
+/* Table: User_information_table                                */
+/*==============================================================*/
+create table User_information_table
+(
+   User_ID              varchar(20) not null,
+   Del_Address_number   varchar(20),
+   User_Name            varchar(10),
+   User_Gender          varchar(10),
+   User_Password        varchar(20),
+   User_PhoneNumber     varchar(20),
+   User_email           varchar(20),
+   Address_number       varchar(20),
+   User_Registration_time datetime,
+   Is_it_a_member       bool,
+   Membership_deadline  datetime,
+   primary key (User_ID)
+);
+
+alter table Bill_collection_and_voucher_delivery_form add constraint FK_Bill_collection_and_voucher_delivery_form foreign key (Bus_Business_ID)
+      references Business_information_table (Business_ID) on delete restrict on update restrict;
+
+alter table Bill_collection_and_voucher_delivery_form add constraint FK_Bill_collection_and_voucher_delivery_form foreign key (Cou_Coupon_ID)
+      references Coupon_information_sheet (Coupon_ID) on delete restrict on update restrict;
+
+alter table Bill_collection_and_voucher_delivery_form add constraint FK_Bill_collection_and_voucher_delivery_form foreign key (Use_User_ID)
+      references User_information_table (User_ID) on delete restrict on update restrict;
+
+alter table Commodity_category_table add constraint FK_Relationship_1 foreign key (Commodity_ID)
+      references Commodity_list (Commodity_ID) on delete restrict on update restrict;
+
+alter table Commodity_category_table add constraint FK_Relationship_3 foreign key (Business_ID)
+      references Business_information_table (Business_ID) on delete restrict on update restrict;
+
+alter table Commodity_evaluation_form add constraint FK_Commodity_evaluation_form foreign key (Bus_Business_ID2)
+      references Business_information_table (Business_ID) on delete restrict on update restrict;
+
+alter table Commodity_evaluation_form add constraint FK_Commodity_evaluation_form foreign key (Com_Commodity_ID)
+      references Commodity_list (Commodity_ID) on delete restrict on update restrict;
+
+alter table Commodity_evaluation_form add constraint FK_Commodity_evaluation_form foreign key (Use_User_ID)
+      references User_information_table (User_ID) on delete restrict on update restrict;
+
+alter table Commodity_evaluation_form add constraint FK_Commodity_evaluation_form2 foreign key (Bus_Business_ID)
+      references Business_information_table (Business_ID) on delete restrict on update restrict;
+
+alter table Commodity_order add constraint FK_Commodity_order foreign key (Bus_Business_ID)
+      references Business_information_table (Business_ID) on delete restrict on update restrict;
+
+alter table Commodity_order add constraint FK_Commodity_order foreign key (Commodity_ID)
+      references Commodity_list (Commodity_ID) on delete restrict on update restrict;
+
+alter table Commodity_order add constraint FK_Commodity_order foreign key (Cou_Coupon_ID)
+      references Coupon_information_sheet (Coupon_ID) on delete restrict on update restrict;
+
+alter table Commodity_order add constraint FK_Commodity_order foreign key (Ful_Full_minus_ID2)
+      references Full_reduction_scheme_table (Full_minus_ID) on delete restrict on update restrict;
+
+alter table Commodity_order add constraint FK_Commodity_order foreign key (Rid_Rider_ID)
+      references Rider_information_sheet (Rider_ID) on delete restrict on update restrict;
+
+alter table Commodity_order add constraint FK_Commodity_order foreign key (Use_User_ID)
+      references User_information_table (User_ID) on delete restrict on update restrict;
+
+alter table Commodity_order add constraint FK_Commodity_order2 foreign key (Cou_Coupon_ID2)
+      references Coupon_information_sheet (Coupon_ID) on delete restrict on update restrict;
+
+alter table Commodity_order add constraint FK_Commodity_order2 foreign key (Ful_Full_minus_ID)
+      references Full_reduction_scheme_table (Full_minus_ID) on delete restrict on update restrict;
+
+alter table Coupon_holding_form add constraint FK_Coupon_holding_form foreign key (Bus_Business_ID)
+      references Business_information_table (Business_ID) on delete restrict on update restrict;
+
+alter table Coupon_holding_form add constraint FK_Coupon_holding_form foreign key (Use_User_ID)
+      references User_information_table (User_ID) on delete restrict on update restrict;
+
+alter table Rider_entry add constraint FK_Rider_entry foreign key (Ord_Order_ID)
+      references Order_details (Order_ID) on delete restrict on update restrict;
+
+alter table Rider_entry add constraint FK_Rider_entry foreign key (Rid_Rider_ID)
+      references Rider_information_sheet (Rider_ID) on delete restrict on update restrict;
+
+alter table Rider_entry add constraint FK_Rider_entry foreign key (User_ID)
+      references User_information_table (User_ID) on delete restrict on update restrict;
+
+alter table User_information_table add constraint FK_Relationship_2 foreign key (Del_Address_number)
+      references Delivery_address_list (Address_number) on delete restrict on update restrict;
+
