eb7ce1a7b5bd4c117499dd71f62f3e26381854c5
1798689040 <67767410+1798689040@users.noreply.github.com>
2020-07-15 06:11:50 +0800

    Add files via upload

---
+++ DBUtil.java
@@ -0,0 +1,24 @@
+package cn.edu.zucc.take_away.util;
+
+import java.sql.Connection;
+
+import java.sql.Connection;
+
+public class DBUtil {
+	private static final String jdbcUrl="jdbc:mysql://localhost:3306/take_away?useUnicode=true&characterEncoding=UTF-8&useSSL=false";
+	private static final String dbUser="root";
+	private static final String dbPwd="1234";
+	public static Object usermanager;
+	public static Object orderManager;
+	static{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static Connection getConnection() throws java.sql.SQLException{
+		return java.sql.DriverManager.getConnection(jdbcUrl, dbUser, dbPwd);
+	}
+}
